d7cf90b2931b64db356dd3a8fc23221353c92e54
Sricharan <sricharanized@gmail.com>
2018-11-25 02:54:14 +0100

    changes

--- tourcyst.py
+++ tourcyst.py
@@ -16,17 +16,19 @@ def autocomplete():
     data = get_autocomplete(query)
     return jsonify(data)
 
+
 @app.route('/search')
 def search():
     oName = request.args.get('from', '')
     dName = request.args.get('to', '')
-    print(oName, dName)
+    # print(oName, dName)
     if(oName and dName):
-      data = get_all_search(oName, dName)
+        data = get_all_search(oName, dName)
     else:
-      data = "Specify from and to locations"
+        data = "Specify from and to locations"
     return jsonify(data)
-  
+
+
 @app.route('/geocode')
 def geocode():
     query = request.args.get('query', '')
@@ -36,36 +38,38 @@ def geocode():
 
 @app.route('/fastestplace/')
 def fastestplace():
-	place1 = request.args.get('place1', '')
-	place2 = request.args.get('place2', '')
-	place3 = request.args.get('place3', '')
-	data = get_fastest_central(place1, place2, place3)
-	return jsonify(data)
+    place1 = request.args.get('place1', '')
+    place2 = request.args.get('place2', '')
+    place3 = request.args.get('place3', '')
+    data = get_fastest_central(place1, place2, place3)
+    # print(data)
+    return jsonify(data)
 
 
 @app.route('/cheapestplace/')
 def cheapestplace():
-	place1 = request.args.get('place1', '')
-	place2 = request.args.get('place2', '')
-	place3 = request.args.get('place3', '')
-	data = get_cheapest_central(place1, place2, place3)
-	return jsonify(data)
+    place1 = request.args.get('place1', '')
+    place2 = request.args.get('place2', '')
+    place3 = request.args.get('place3', '')
+    data = get_cheapest_central(place1, place2, place3)
+    # print("hello" + data)
+    return jsonify(data)
 
 
 @app.route('/allplaces/')
 def findall():
-	place1 = request.args.get('place1', '')
-	place2 = request.args.get('place2', '')
-	place3 = request.args.get('place3', '')
-	data = get_places(find_centriod(place1, place2, place3))
-	return jsonify(data)
+    place1 = request.args.get('place1', '')
+    place2 = request.args.get('place2', '')
+    place3 = request.args.get('place3', '')
+    data = get_places(find_centriod(place1, place2, place3))
+    return jsonify(data)
 
 
 @app.route('/placedetails/')
 def placedetails():
-	place = request.args.get('place', '')
-	data = get_geocode(place)
-	return jsonify(data)
+    place = request.args.get('place', '')
+    data = get_geocode(place)
+    return jsonify(data)
 
 
 # Helper fuctions
@@ -85,11 +89,12 @@ def get_fastest_central(place1, place2, place3):
         transit_map["total_transit"] = transit1 + transit2 + transit3
         transits.append(transit_map)
     best_location = get_fastest_transit_central(transits)
-	return best_location
+    return best_location
 
 
 def get_cheapest_central(place1, place2, place3):
-    central = get_places(find_centriod(place1, place2, place3))
+    central = get_places(find_centriod(
+        place1, place2, place3), place1, place2, place3)
     transits = []
     for c in central:
         transit_map = {}
@@ -101,7 +106,10 @@ def get_cheapest_central(place1, place2, place3):
             "routes"][0]["indicativePrices"][0]["price"]
         transit_map["name"] = c["name"]
         transit_map["total_price"] = price1 + price2 + price3
+        print("name: " + c["name"] + ", " +
+              "price: " + str(price1 + price2 + price3))
         transits.append(transit_map)
+
     best_location = get_cheapest_transit_central(transits)
     return best_location
 
@@ -117,20 +125,26 @@ def center_geolocation(geolocations):
     coordinates['lng'] = sum(lng)/len(lng)
     return coordinates
 
+
 def get_geocode(query):
-    payload = {"key":rome2rio_key, "query":query}
-    response = requests.get("http://free.rome2rio.com/api/1.4/json/Geocode", params=payload)
+    payload = {"key": rome2rio_key, "query": query}
+    response = requests.get(
+        "http://free.rome2rio.com/api/1.4/json/Geocode", params=payload)
     return response.json()
 
+
 def get_all_search(place1, place2):
-    payload = {"key":rome2rio_key, "oName" : place1, "dName": place2}
-    response = requests.get("http://free.rome2rio.com/api/1.4/json/Search", params=payload)
+    payload = {"key": rome2rio_key, "oName": place1, "dName": place2}
+    response = requests.get(
+        "http://free.rome2rio.com/api/1.4/json/Search", params=payload)
     data = response.json()
     return data
 
+
 def get_autocomplete(query):
-    payload = {"key":rome2rio_key, "query":query}
-    response = requests.get("http://free.rome2rio.com/api/1.4/json/Autocomplete", params=payload)
+    payload = {"key": rome2rio_key, "query": query}
+    response = requests.get(
+        "http://free.rome2rio.com/api/1.4/json/Autocomplete", params=payload)
     data = response.json()
     return data
 
@@ -139,47 +153,67 @@ def find_centriod(place1, place2, place3):
     place1_loc = get_geocode(place1)
     place2_loc = get_geocode(place2)
     place3_loc = get_geocode(place3)
-    place1_pair = (place1_loc["places"][0]["lat"], place1_loc["places"][0]["lng"])
-    place2_pair = (place2_loc["places"][0]["lat"], place2_loc["places"][0]["lng"])
-    place3_pair = (place3_loc["places"][0]["lat"], place3_loc["places"][0]["lng"])
+    place1_pair = (place1_loc["places"][0]["lat"],
+                   place1_loc["places"][0]["lng"])
+    place2_pair = (place2_loc["places"][0]["lat"],
+                   place2_loc["places"][0]["lng"])
+    place3_pair = (place3_loc["places"][0]["lat"],
+                   place3_loc["places"][0]["lng"])
     # print(place1_pair, place2_pair, place3_pair)
     central = center_geolocation([place1_pair, place2_pair, place3_pair])
     # print(central)
     return central
 
 
-def get_places(pnt):
+def get_places(pnt, place1, place2, place3):
     lat_lng_str = str(pnt['lat']) + ',' + str(pnt["lng"])
-    payload = {"location": lat_lng_str,"radius":500,"type":"airports","key": "AIzaSyCNa0G19BABRTzrn2AyO6VyClwhM3iilOw"}
-	
-    #response = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json", params=payload)
+    payload = {"location": lat_lng_str, "radius": 500, "type": "airports",
+               "key": "AIzaSyCNa0G19BABRTzrn2AyO6VyClwhM3iilOw"}
+
+    # response = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json", params=payload)
     # place = {"name": response.json()['results'][0]["name"], "lat": response.json()['results'][0]["geometry"]["location"]["lat"], "lng": response.json()['results'][0]["geometry"]["location"]["lng"]}
     # print(place)
-    #data = response.json()
-	
-	city_list_df = pd.read_csv('biggest_cities.csv', sep =";")
-	range = 5
-	l=[]
+    # data = response.json()
+    city_list_df = pd.read_csv('biggest_cities.csv', sep=";")
+    city_list_df = city_list_df[(city_list_df.name != place1) &
+                                (city_list_df.name != place2) &
+                                (city_list_df.name != place3)]
+    rnge = 10
+    l = []
     test_df = city_list_df
-	lat_0 = pnt['lat']
-	lng_0 = pnt['lng']
-    test_df = test_df[(test_df.lat >lat_0 - range) & (test_df.lat <lat_0 + range)]
-    test_df = test_df[(test_df.lng >lng_0 - range) & (test_df.lng <lng_0 + range)]
+    lat_0 = pnt['lat']
+    lng_0 = pnt['lng']
+    test_df = test_df[(test_df.lat > lat_0 - rnge) &
+                      (test_df.lat < lat_0 + rnge)]
+    test_df = test_df[(test_df.lng > lng_0 - rnge) &
+                      (test_df.lng < lng_0 + rnge)]
+    print(test_df)
     possible_pts = test_df.reset_index()
-    for index,row in possible_pts.iterrows():
+    for index, row in possible_pts.iterrows():
         l += [{'lat': row['lat'], 'lng': row['lng'], 'name': row['name']}]
-	
-	response = l
-	data=response.json()
+
+    response = l
+#    data=response.json()
+    data = response
     places = []
-    length=len(data['results'])
+    length = len(data)
     for i in range(length):
         dict = {}
-        dict={'lat': data["results"][i]["geometry"]["location"]["lat"], 'lng':data["results"][i]["geometry"]["location"]["lng"], 
-                 'name':data["results"][i]["name"]}
+        dict = {'lat': data[i]["lat"], 'lng': data[i]["lng"],
+                'name': data[i]["name"]}
         places.append(dict)
-    print("Places: " + str(places))
+    # print("Places: " + str(data))
     return places
+
+#    return response
+#    length=len(data['results'])
+#    for i in range(length):
+#        dict = {}
+#        dict={'lat': data["results"][i]["geometry"]["location"]["lat"], 'lng':data["results"][i]["geometry"]["location"]["lng"],
+#                 'name':data["results"][i]["name"]}
+#        places.append(dict)
+#    print("Places: " + str(places))
+#    return places
 
 
 def get_fastest_transit_central(transits):
@@ -206,19 +240,19 @@ def get_metrics_for_search(place1, place2):
     metrics = []
     routes = get_all_search(place1, place2)["routes"]
     for i in range(len(routes)):
-      route = {}
-      route["id"] = i
-      route["name"] = routes[i]["name"]
-      route["totalDuration"]  = routes[i]["totalDuration"]
-      route["totalTransitDuration"]  = routes[i]["totalTransitDuration"]
-      route["indicativePrice"] = routes[i]["indicativePrices"][0]["price"]
-      metrics.append(route)
-    return metrics    
-
+        route = {}
+        route["id"] = i
+        route["name"] = routes[i]["name"]
+        route["totalDuration"] = routes[i]["totalDuration"]
+        route["totalTransitDuration"] = routes[i]["totalTransitDuration"]
+        route["indicativePrice"] = routes[i]["indicativePrices"][0]["price"]
+        metrics.append(route)
+    return metrics
 
 
 def get_recommended_central(place1, place2, place3):
-    centrals = get_places(find_centriod(place1, place2, place3))
+    centrals = get_places(find_centriod(
+        place1, place2, place3), place1, place2, place3)
     # TODO: Recommender logic goes here
     return central
 
@@ -245,16 +279,10 @@ def get_recommended_central(place1, place2, place3):
 #     print("Distances from each place is " + str(best_cost_from_place1) + " " + str(best_cost_from_place2) + " " + str(best_cost_from_place3) + "minutes respectively")
 
 
-
-
-
-
-
 # find_centriod("Munich", "Madrid", "Paris")
-get_cheapest_central("Munich", "Madrid", "Paris")
+print(get_cheapest_central("Munich", "Madrid", "Paris"))
 # get_fastest_route("Munich", "Madrid", "Paris")
 # print(get_metrics_for_search("Munich", "Paris"))
-
 
 
 # # TODO: Rewrite to get_best_destination(place1, place2, place3)
